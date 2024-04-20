dafba0d9f7f4ad105fc83508b48b97203e0eefe4
ahippoly <ahippoly@student.42.fr>
2024-04-20 13:05:23 +0200

    added openzeppelin locally

--- CR8Art.sol
+++ CR8Art.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "./lib/openzeppelin/token/ERC721/ERC721.sol";
+import {Ownable} from "./lib/openzeppelin/access/Ownable.sol";
+import {ERC721URIStorage} from "./lib/openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
 
 // CR8 Art NFT 
 
--- CR8Cert.sol
+++ CR8Cert.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.24;
 
 //NFT for Datasets & Model
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "./lib/openzeppelin/token/ERC721/ERC721.sol";
+import {Ownable} from "./lib/openzeppelin/access/Ownable.sol";
+import {ERC721URIStorage} from "./lib/openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
 
 enum datatype {
     images,
